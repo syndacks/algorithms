1e85e788d38fcc7483ac00ee87430ed95d12ba87
Dacks <math.milliken@gmail.com>
2017-01-13 16:08:42 -0500

    Add files via upload

--- app2.js
+++ app2.js
@@ -1,51 +1,99 @@
-// require underscore library
-var _ = require('underscore')
-// require exec
-var exec = require('child_process').exec;
+var request = require("request");
+var cheerio = require("cheerio");
+var http = require("http");
+var fs = require("fs");
+
+var videoSource = "http://av.voanews.com/Videoroot/Pangeavideo/2016/07/0/0c/0c3de39d-d832-4e44-a193-93171040cebf_hq.mp4"
+
+var globalArray = [
+"http://learningenglish.voanews.com/a/3435564.html",
+"http://learningenglish.voanews.com/a/3605385.html",
+"http://learningenglish.voanews.com/a/3620609.html",
+"http://learningenglish.voanews.com/a/3563804.html",
+"http://learningenglish.voanews.com/a/3240171.html",
+"http://learningenglish.voanews.com/a/3605387.html",
+"http://learningenglish.voanews.com/a/3545534.html",
+"http://learningenglish.voanews.com/a/3406302.html",
+"http://learningenglish.voanews.com/a/3563798.html"
+];
+
+var vidsToDownload = [
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/11/d/dc/dc28b28a-8a3d-42b4-bbaf-7a3710ccff11_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/10/a/a3/a3059e29-a602-492d-918d-f9ac7122a33a_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/11/c/c2/c2ead044-e9cc-4834-ba20-2164ef45f4d3_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/10/6/69/691bff73-3e7d-4535-a1c4-225bfa7543d4_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/07/0/0c/0c3de39d-d832-4e44-a193-93171040cebf_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/07/d/d8/d84d89f5-c535-4bec-be63-a6514b462f10_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/12/6/64/6418b8b2-f6e1-423e-8579-fe5d3a408ec1_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/10/f/f3/f38dfd96-7be5-42df-8739-6449311e6c28_hq.mp4',
+'http://av.voanews.com/Videoroot/Pangeavideo/2016/03/e/ee/eef9adba-fac6-4436-87a0-fc22c282915e_hq.mp4'
+];
+
+var vidShowPage = 'http://learningenglish.voanews.com/z/986/pc13.html';
+
+// download(videoSource, 'videoTest45.mp4');
 
 
-// require each app's db.json file, if can't be found, throw error
-try{
-	var data = require('./content/db.json');
-	// var templateType = require("./app.yaml");
-} catch (err) {
-	getCurrentDirectoryName();
-	return;
+function scrapeEpisodeUrls () {
+    request(vidShowPage, function(error, response, body) {
+        if (!error) {
+            var $ = cheerio.load(body);
+            // get all the showPage URLS
+            $('div.media-block.with-date.width-img.size-3 a.img-wrapper').filter(function() {
+                var titleText = $(this).attr('title');
+                var url = $(this).attr('href');
+                var fixedURL = "http://learningenglish.voanews.com" + url;
+                var myString = titleText + ":::" + fixedURL;
+                globalArray.push({
+                	key: fixedURL,
+                	videoURL: "",
+                	videoTitle: "",
+                	videoDescription: ""
+                });
+            })
+        }
+    })
 }
 
-// determines number of articles that belong to each tag using _.countBy method
-var categoryData = _.countBy(data.content, function(data) { return data.tags; });
-
-//count categories
-var count = 0;
-for (k in categoryData) if (categoryData.hasOwnProperty(k)) count++;
-
-//print it all out
-console.log("---")
-	//app title
-	console.log("AppId: " + _.values(data)[0])
-	//category info
-	console.log("Number of Categories: " + count);
-	console.log("Category/Article data: ");
-	console.log(categoryData);
+// scrapeEpisodeUrls();
 
 
-// get file contents of 'app.yaml'
-function puts(error, stdout, stderr){
-	console.log("Template Type:" + (stdout))
-	}
-	exec("cat app.yaml", puts);
-
-// function to print working directory if catch error above
-function getCurrentDirectoryName(){
-	var fullPath = __dirname; 
-	var path = fullPath.split('/'); 
-	var cwd = path[path.length-1]; 
-	console.log(cwd);
+function getVideoUrlFromShowPage() {
+    for (var i = 0; i < globalArray.length; i++) {
+        request(globalArray[i], function(error2, response2, body2) {
+            if (!error2) {
+                var $$ = cheerio.load(body2);
+                $$('div.player-and-links video').filter(function() {
+                    var videoURL = $$(this).attr('src');
+                    var videoURL_HQ = videoURL.slice(0, -4) + "_hq" + videoURL.slice(-4);
+                    console.log("'"+videoURL_HQ+"'"+",");
+                });
+            }
+        })
+    }
 }
 
-// bash script to copy app.js to all subdirectories
-// for d in */; do cp app.js "$d"; done
+// getVideoUrlFromShowPage();
 
-// bash script to run in parent directory
-// for d in com.endlessm.*?/; do (cd $d && node ./app.js); done
+
+function download (url, dest, cb) {
+    var file = fs.createWriteStream(dest);
+    var request = http.get(url, function(response) {
+        response.pipe(file);
+        file.on('finish', function() {
+            file.close(cb); // close() is async, call cb after close completes.
+        });
+    }).on('error', function(err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        if (cb) cb(err.message);
+    });
+}
+
+
+for (i=1;i<vidsToDownload.length; i++){
+    filename= "./videos/file"+i+".mp4";
+    download(vidsToDownload[i], filename);
+    }
+
+
+
